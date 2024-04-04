ad555dbfd559ee36e70aa1edbda1854a05ee0b15
akedarig <akedari66@gmail.com>
2024-04-04 08:41:49 +0530

    commited

---
+++ MapAssignmet.java
@@ -0,0 +1,50 @@
+package Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapAssignmet {
+	
+	String str="";
+	public void m() throws IOException
+	{
+		FileReader file=new FileReader("C:\\Users\\ANIL\\eclipse-workspace\\Selenium4\\TestData");
+		BufferedReader br = new BufferedReader(file);
+		
+			while ((str=br.readLine())!=null)
+			{
+				//System.out.println(str);
+				
+				HashMap<String, String> map=new HashMap<String, String>();
+				String [] keyvalue = str.split(":");
+				map.put(keyvalue[0], keyvalue[1]);
+				
+				System.out.println(map);
+				
+				/*for (Map.Entry<String, String> entry : 
+		             map.entrySet()) { 
+		            System.out.println(entry.getKey() + " : "
+		                               + entry.getValue());
+				
+			}*/
+			}
+			
+}
+	public static void main(String[] args) {
+		
+		MapAssignmet ma= new MapAssignmet();
+		try {
+			ma.m();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+	}
+
+
